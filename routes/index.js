d7692e87c317b9aeb9087e5c8f6b6defe6799612
courtneysanders418 <cswoods88@gmail.com>
2016-10-21 15:33:55 -0600

    CRUD complete

--- routes/index.js
+++ routes/index.js
@@ -24,49 +24,52 @@ router.get('/signup', function(req, res) {
     });
 });
 
-
-// sending login info to the database
-router.post('/login', function(req, res) {
-    knex('users').where('email', req.body.email).then(function(results) {
-        console.log(results);
-        if (results.length == 0) {
-            res.render('error', {
-                message: 'email or password incorrect.'
-            })
-        } else {
-            var user = results[0];
-            var passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password)
-            delete user.hashed_password;
-            if (passwordMatch === false) {
-                res.render('error', {
-                    message: 'email or password incorrect.'
-                })
-            } else {
-                req.session.userInfo = user
-                res.redirect('/posts')
-                console.log(user);
-            }
-        }
-    })
-})
-
-
-// sending user info to the database
-router.post('/signup', function(req, res) {
-    console.log(req.body);
-    knex('users')
-        .insert({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            username: req.body.username,
-            email: req.body.email,
-            hashed_password: bcrypt.hashSync(req.body.password, 12)
-        }, '*')
-        .then(function(insertionData) {
-            console.log('inserted data worked', insertionData);
-            res.redirect('/posts')
-        })
+router.get('/logout', function(req, res) {
+            req.session = null;
+            res.redirect('/')
 });
+            // sending login info to the database
+            router.post('/login', function(req, res) {
+                knex('users').where('email', req.body.email).then(function(results) {
+                    console.log(results);
+                    if (results.length == 0) {
+                        res.render('error', {
+                            message: 'email or password incorrect.'
+                        })
+                    } else {
+                        var user = results[0];
+                        var passwordMatch = bcrypt.compareSync(req.body.password, user.hashed_password)
+                        delete user.hashed_password;
+                        if (passwordMatch === false) {
+                            res.render('error', {
+                                message: 'email or password incorrect.'
+                            })
+                        } else {
+                            req.session.userInfo = user
+                            res.redirect('/posts')
+                            console.log(user);
+                        }
+                    }
+                })
+            })
 
 
-module.exports = router;
+            // sending user info to the database
+            router.post('/signup', function(req, res) {
+                console.log(req.body);
+                knex('users')
+                    .insert({
+                        first_name: req.body.first_name,
+                        last_name: req.body.last_name,
+                        username: req.body.username,
+                        email: req.body.email,
+                        hashed_password: bcrypt.hashSync(req.body.password, 12)
+                    }, '*')
+                    .then(function(insertionData) {
+                        console.log('inserted data worked', insertionData);
+                        res.redirect('/posts')
+                    })
+            });
+
+
+            module.exports = router;
