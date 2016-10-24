c2099d3112f1a571004bb8d3a5b2433526c8a3ee
courtneysanders418 <cswoods88@gmail.com>
2016-10-24 16:55:45 -0600

    procfile added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cookieSession = require('cookie-session')
-
+var favicon = require('favicon');
 var app = express();
 
 if (process.env.NODE_ENV !== 'production') {
